81657f52d218ebdac7d658d90b1e827f15ed2374
Sem Rekkers <sem@devments.com>
2015-09-17 14:46:02 +0200

    src: create error.c

---
+++ src/error.c
@@ -0,0 +1,10 @@
+#ifndef TRIBBLE_ERROR_H_
+#define TRIBBLE_ERROR_H_
+
+#include "tribble.h"
+
+const Error E_NOMEM = "out of memory";
+const Error E_INVAL = "invalid argument";
+const Error E_NOBUF = "buffer is empty";
+
+#endif // TRIBBLE_ERROR_H_
